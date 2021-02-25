26b53bf712473c562077a4c280ddbbd733024940
Rafael <rafaelfx153@gmail.com>
2021-02-25 04:47:34 -0300

    modificacao 25 de fevereiro

--- invoice_atv.py
+++ invoice_atv.py
@@ -113,7 +113,7 @@ while n == 0:
 """
 #2ª tentativa
 m = 0 
-
+valor_total = []
 while n < 1000 :
     print("Passe o produto \n")
     a = int(input ("informe o código \n"))
@@ -125,6 +125,7 @@ while n < 1000 :
     d = float(input("qual o preço \n"))
     produto.set_preco(d)
     lista.append(produto)
+    valor_total.append(d*c)
     Nota_escrita('notafiscal.txt' ,produto.get_numero(), produto.get_descricao(), produto.get_quantidade(), produto.get_preco(), produto.getInvoiceAmout())
    
     print("se desejar continuar digite 0 /n")
@@ -136,6 +137,26 @@ while n < 1000 :
     n = n + 1
 
 #print(lista[0].get_preco())
+valor = sum(valor_total)
+print("O valor das suas compras foi de: R$ ", valor)
+
+
+
+
+#forma de pagamento
+#qual sua forma de pagamento ? 
+print("Qual sua forma de pagamento?")
+print("1 - Cartão| 2 - Dinheiro| 3- Pix \n")
+j = int(input("Digite aqui sua forma de pagamento \n"))
+
+if (j== 1):
+    print("Você pagou no cartão")
+elif(j == 2):
+    print("Voce pagou no dinheiro")    
+elif(j == 3):
+    print("Voce pagou no pix")
+else:    
+    print("voce não digitou nada abestado")
 
 
 
@@ -148,6 +169,19 @@ while n < 1000 :
 
 
 #agora vai passar na maquininha bip bip 
+
+#salvar forma de pagamento e calcular o pagamento total
+
+#calcular pagamento total 
+#acho que dá de usar uma lista
+#bora ver 
+#valor_total = []
+#valor_total.append()#aqui informar o valor 
+#depois rodar um while
+
+#for(n in valor_total):
+#y = sum(valor_total)
+
 
 
 
